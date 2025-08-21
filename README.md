c352a668c49f5d6900b490e1a87e0a52eaf675fc
Jess Yen <jess.yen@ideku.io>
2025-08-21 11:05:49 +0800

    compose

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 #### branch: traditional-layout
 #### branch: compose
+<video src="video/Screen_recording_20250821_110056.mp4" controls width="400"></video>
--- app/src/main/java/com/id/basicviewmodelexample/MainActivity.kt
+++ app/src/main/java/com/id/basicviewmodelexample/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.id.basicviewmodelexample.ui.theme.BasicViewModelExampleTheme
@@ -25,18 +33,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.tv_count)
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.count.collect {
-                    textView.text = it.toString()
-                }
+        setContent {
+            BasicViewModelExampleTheme {
+                MainScreen(viewModel)
             }
         }
-        findViewById<TextView>(R.id.btn_increment).setOnClickListener {
-            viewModel.increment()
+    }
+}
+
+@Composable
+fun MainScreen(viewModel: MainViewModel) {
+    val count = viewModel.count.collectAsStateWithLifecycle()
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = count.value.toString(),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { viewModel.increment() }) {
+                Text("Increment")
+            }
         }
     }
 }
